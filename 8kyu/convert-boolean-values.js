fd508cb7d2ccfa9721c3de9db00cdb7917089758
anbogod <pulseczar99@gmail.com>
2026-02-08 18:20:15 +0500

    complete convent-boolean-values

---
+++ 8kyu/convert-boolean-values.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// TESTS:
+
+//  assert.strictEqual(boolToWord(true), 'Yes')
+//  assert.strictEqual(boolToWord(false), 'No')
+
+// SOLUTION:
+
+
+function boolToWord( bool ){
+  //...
+  return bool ? 'Yes' : 'No';
+}
+
+console.log(boolToWord( false ));
