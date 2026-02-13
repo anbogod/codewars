bf42922b29481582e1284f2b9cd9735106d15bf6
anbogod <pulseczar99@gmail.com>
2026-02-13 20:25:14 +0500

    complete beginner-lost

---
+++ 8kyu/beginner-lost .js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+//Given an array of integers, return a new array with each value doubled.
+
+
+// TESTS:
+
+// assert.deepEqual(maps([1, 2, 3]), [2, 4, 6]);
+// assert.deepEqual(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8]); 
+// assert.deepEqual(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4]); 
+// SOLUTION:
+
+function maps(x) {
+
+  let y = []
+  for (let i = 0; i < x.length; i++) {
+    y.push(x[i] * 2)
+
+  }
+
+  return y
+}
+
+console.log(maps(x));
+
+
---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,32 @@
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// For example:
+
+// TESTS:
+
+// assert.strictEqual(solution('world'), 'dlrow');
+// assert.strictEqual(solution('hello'), 'olleh');
+// assert.strictEqual(solution(''), '');
+// assert.strictEqual(solution('h'), 'h');
+
+// SOLUTION:
+
+function evenOrOdd(number) {
+
+  return number % 2 === 0 ? "Even" : "Odd"
+
+  // if (number % 2 === 0)  {
+  //     return "Even"
+  //   } else {
+  //     return "Odd"
+  //   }
+
+}
+
+console.log(evenOrOdd(number));
+
+
